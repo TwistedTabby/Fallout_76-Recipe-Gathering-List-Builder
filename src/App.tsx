e8ce00096e099df3dd6e8793d36759e47b156303
Rebecca Dessonville <rebecca@purrlydigital.com>
2025-02-10 15:28:35 -0600

    Starting over homepage

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          {/* Other routes will be added here */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App; 
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,8 +1,51 @@
 export const strings = {
-  appTitle: 'FO76 Recipe Manager',
+  appTitle: 'FO76 Gathering List',
   nav: {
     home: 'Home',
     recipes: 'Recipes',
-    import: 'Import Recipe'
+    gatheringList: 'Gathering List'
+  },
+  categories: {
+    food: 'Food',
+    chems: 'Chems'
+  },
+  filters: {
+    title: 'Filters',
+    category: 'Category',
+    buffCategory: 'Buff Category',
+    type: 'Type',
+    clearAll: 'Clear All',
+    apply: 'Apply Filters'
+  },
+  special: {
+    strength: 'Strength',
+    perception: 'Perception',
+    endurance: 'Endurance',
+    charisma: 'Charisma',
+    intelligence: 'Intelligence',
+    agility: 'Agility',
+    luck: 'Luck'
+  },
+  gatheringList: {
+    title: 'Gathering List',
+    viewOptions: {
+      allCategories: 'All Categories',
+      specificCategory: 'Specific Category',
+      allTypes: 'All Types',
+      specificType: 'Specific Type'
+    },
+    empty: 'No recipes selected',
+    totalIngredients: 'Total Ingredients Needed',
+    addRecipe: 'Add to Gathering List',
+    removeRecipe: 'Remove from Gathering List',
+    clearList: 'Clear List'
+  },
+  recipe: {
+    ingredients: 'Ingredients',
+    buffs: 'Buffs',
+    types: 'Types',
+    category: 'Category',
+    duration: 'Duration',
+    quantity: 'Quantity'
   }
 }; 

1c36eca668e5adf03d2796ebae9b802524cc0709
Rebecca Dessonville <rebecca@purrlydigital.com>
2025-02-10 16:03:02 -0600

    Changes made 2025-02-10 16:03:02

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,16 @@
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: './', // This ensures assets are loaded correctly in production
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+}) 

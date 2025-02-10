11816e17db6e3c611f40a792433288ea58d83a8b
Rebecca Dessonville <rebecca@purrlydigital.com>
2025-02-10 12:02:29 -0600

    Deleting front end related files to start over.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import RecipeList from './pages/RecipeList';
-import GatheringList from './pages/GatheringList';
-import { ImportRecipe } from './pages/ImportRecipe';
-import { AuthProvider } from './context/AuthContext';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import Login from './pages/Login';
-import Home from './pages/Home';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipe-list" element={<RecipeList />} />
-            <Route path="/gathering-list" element={<GatheringList />} />
-            <Route 
-              path="/import" 
-              element={
-                <ProtectedRoute requiresCollaborator>
-                  <ImportRecipe />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </AuthProvider>
+    <div>
+      <h1>Recipe Manager</h1>
+    </div>
   );
 }
 

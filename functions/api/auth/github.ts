ad07bd2539ba0db9ce07bdf4a8f5e79bc1d54c49
Rebecca Dessonville <rebecca@purrlydigital.com>
2025-02-11 11:18:19 -0600

    Changes made 2025-02-11 11:18:19

--- functions/api/auth/github.ts
+++ functions/api/auth/github.ts
@@ -2,7 +2,18 @@ export async function onRequestGet(context) {
   const clientId = context.env.GITHUB_CLIENT_ID;
   const redirectUri = `${context.env.BASE_URL}/api/auth/callback`;
   
-  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:org`;
+  // Get the state parameter from the request URL
+  const url = new URL(context.request.url);
+  const state = url.searchParams.get('state');
   
-  return Response.redirect(url, 302);
+  // Construct GitHub OAuth URL with state if present
+  const githubUrl = new URL('https://github.com/login/oauth/authorize');
+  githubUrl.searchParams.set('client_id', clientId);
+  githubUrl.searchParams.set('redirect_uri', redirectUri);
+  githubUrl.searchParams.set('scope', 'read:org');
+  if (state) {
+    githubUrl.searchParams.set('state', state);
+  }
+  
+  return Response.redirect(githubUrl.toString(), 302);
 } 
